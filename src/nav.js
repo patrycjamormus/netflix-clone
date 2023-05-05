55eb97529a507925ecdb5e1cf90a039799b968f4
patrycjamormus <106437709+patrycjamormus@users.noreply.github.com>
2023-05-05 21:59:19 +0200

    nav

--- src/nav.js
+++ src/nav.js
@@ -2,15 +2,24 @@ import React, { useEffect, useState } from "react";
 import "./nav.css";
 
 function Nav() {
-
+const [show, handleShow] = useState (false);
 
 useEffect (() => {
-    
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll",null );
+    };
 }, []);
+
+/*funkcja window okresla kiedy pasek z przezroczystego ma sie zmienic na czarny*/
 
 
   return (
-    <div className={`nav`}> {/*zawsze nav ale jesli show jest true to czasowo zamien to na nav_black */}
+    <div className={`nav ${show && "nav_black"}`}> {/*zawsze nav ale jesli show jest true to czasowo zamien to na nav_black */}
       <img
         className="nav_logo"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/800px-Netflix_2015_logo.svg.png"
