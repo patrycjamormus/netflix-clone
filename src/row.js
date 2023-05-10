8fc85138d9eb02d3b4f32f4655c4669f554d44bc
patrycjamormus <106437709+patrycjamormus@users.noreply.github.com>
2023-05-10 14:59:49 +0200

    up

--- src/row.js
+++ src/row.js
@@ -1,42 +1,69 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const base_URL = "https://image.tmdb.org/t/p/original";
 
-function Row({title, fetchUrl, isLargeRow}) {
-const [movies, setMovies] = useState([]);
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
-useEffect (() => {
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      //zapytanie zwraca:"https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213"//
+      setMovies(request.data.results);
+      console.log(request);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
 
-async function fetchData (){
-    const request = await axios.get(fetchUrl);
-    //zapytanie zwraca:"https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213"//
-    setMovies (request.data.results);
-    console.log(request);
-    return request;
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.title || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+        setTrailerUrl (urlParams.get('v'));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row_posters">
+        {}
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            onClick={() => handleClick(movie)}
+            className={`row__poster ${isLargeRow && "row_posterLarge"}`}
+            src={`${base_URL}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          /> /*wyswietlanie obrazkow jako prostokat a nie poster - isLargeRow + movie.backdrop_path*/
+        ))}
+      </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
 }
-fetchData();
-}, [fetchUrl]);
 
-
-    return (
-        <div className="row"> 
-                <h2>{title}</h2>
-
-                <div className="row_posters">
-                    {}
-                    {movies.map(movie => (
-                        <img 
-                        key={movie.id}
-                        className={`row__poster ${isLargeRow && "row_posterLarge"}`}
-                        src={`${base_URL}${isLargeRow ? movie.poster_path: movie.backdrop_path }`} alt={movie.name}/> /*wyswietlanie obrazkow jako prostokat a nie poster - isLargeRow + movie.backdrop_path*/
-                        
-                        ))}
-                </div>
-        </div>
-    )
-}
-
-export default Row
+export default Row;
 // mozliwosc uzycia w innym pliku tej zmiennej//
